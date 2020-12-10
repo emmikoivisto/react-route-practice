896664d60bdc3c0df50cbf0cf403c9cb1f7fbdb6
emmikoivisto <emetsi@gmail.com>
2020-12-10 11:53:54 +0000

    state added to Pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
 
-
 const Pricing = () => {
-  
-  return(
+
+  const [pricing, setPricing] = useState([
+    {level: "Hobby", cost:0},
+    {level: "Start Up", cost:10},
+    {level: "Small business", cost:100},
+    {level: "Enterprice", cost:1000}
+  ]);
+
+  return (
     <div>
       <h4>Pricing</h4>
       <p>£££££££</p>
     </div>
-    )
+  )
+
 };
 
 export default Pricing;
